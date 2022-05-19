c420082af2883c8e6285196bc8a255b68e34bf1d
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-19 11:02:58 +0200

    page a propos

---
+++ src/views/AproposView.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>A PROPOS</h2>
+  <h4>Qui sommes nous ?</h4>
+  <img src="src/assets/images/image-avatar-morgane.jpg" alt="image bonhomme morgane" />
+  <p>Morgane CHAMBERLIN</p>
+  <img src="src/assets/images/image-avatar-marion.jpg" alt="image bonhomme marion" />
+  <p>Marion GILLOT</p>
+
+  <p>
+    Nous sommes Marion GILLOT et Morgane CHAMBERLIN, toutes deux étudiantes en première année MMi, Métier du Multimédia et de l'Internet à
+    l'IUT de Montbeliard(25)
+  </p>
+  <h4>Pourquoi cette innovation</h4>
+  <p>
+    Fait dans le cadre d'un exercice pédagogique, dont le sujet est de créer une application innovante. C'est pourquoi nous avons penser
+    lier familles et artistes afin d'apporter au dessin de vos enfants, la créativité des artistes.
+  </p>
+  <h4>Comment assurons-nous que ce site reste sur pour nos utilisateurs</h4>
+  <p>
+    Nous surveillons les activités sur le site pour la sécurité de chacun. Malheureusement, comme nous ne pouvons pas être partout en même
+    temps nous avons d'autres mesures:
+  </p>
+  <p>
+    Nous demandons aux utilisateurs de signaler toute personne qui utilise un language ou des images blessantes ou inapropriés (commentaire,
+    message, noms d'utilisateurs, avatars, créations.)
+  </p>
+  <p>
+    Votre enfant devrait égaleemnt utiliser des filtres de language pour l'empêcher d'afficher tout contenu qui pourrait venir d'autres
+    utilisateurs
+  </p>
+</template>
