812edff7638b59ca198f7570680b07e824a7503f
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-20 09:34:35 +0200

    test commit

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -1,22 +1,27 @@
 <template>
-  <h2>A PROPOS</h2>
-  <h4>Qui sommes nous ?</h4>
-  <img src="src/assets/images/image-avatar-morgane.jpg" alt="image bonhomme morgane" />
-  <p>Morgane CHAMBERLIN</p>
-  <img src="src/assets/images/image-avatar-marion.jpg" alt="image bonhomme marion" />
-  <p>Marion GILLOT</p>
+  <img src="src/assets/images/hero_apropos.jpg" alt="hero a propos" />
+  <h4 class="mt-12 mb-12 flex justify-center text-2xl">Qui sommes nous ?</h4>
+  <img class="ml-72 mb-5 h-2" src="src/assets/images/apropos-bas.jpg" alt="petite bare texte" />
+  <div class="flex w-64 flex-nowrap justify-self-center">
+    <img src="src/assets/images/image-avatar-morgane.jpg" alt="image bonhomme morgane" />
+    <img src="src/assets/images/image-avatar-marion.jpg" alt="image bonhomme marion" />
+  </div>
+  <div class="flex flex-nowrap justify-center gap-28">
+    <p class="">Morgane CHAMBERLIN</p>
+    <p>Marion GILLOT</p>
+  </div>
 
-  <p>
+  <p class="mr-20 ml-20 mt-10">
     Nous sommes Marion GILLOT et Morgane CHAMBERLIN, toutes deux étudiantes en première année MMi, Métier du Multimédia et de l'Internet à
     l'IUT de Montbeliard(25)
   </p>
-  <h4>Pourquoi cette innovation</h4>
-  <p>
+  <h4 class="mb-5 mt-8 flex justify-center text-2xl">Pourquoi cette innovation</h4>
+  <p class="mr-20 ml-20 mt-10">
     Fait dans le cadre d'un exercice pédagogique, dont le sujet est de créer une application innovante. C'est pourquoi nous avons penser
     lier familles et artistes afin d'apporter au dessin de vos enfants, la créativité des artistes.
   </p>
-  <h4>Comment assurons-nous que ce site reste sur pour nos utilisateurs</h4>
-  <p>
+  <h4 class="mb-3 mt-8 flex justify-center text-2xl">Comment assurons-nous que ce site reste sur pour nos utilisateurs</h4>
+  <p class="ml-14 mr-14 mt-10">
     Nous surveillons les activités sur le site pour la sécurité de chacun. Malheureusement, comme nous ne pouvons pas être partout en même
     temps nous avons d'autres mesures:
   </p>
