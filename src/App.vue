c440e2126946022ad943653a06654a6ef5767a41
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-23 21:17:33 +0200

    changement app et coloriage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <header class="flex bg-gray-100 mb-7">
-  <nav class="flex">
-  
-  <IconLogo class="w-12 h-10 mx-8 my-5" />
-   
-  <div class="flex mt-7 space-x-8 mx-6 font-roboto">
-    <h3 class="block">Acceuil</h3>
-    <h3 class="block">À propos</h3>
-    <h3 class="block">Contact</h3>
-    <h3 class="block">Nos artistes</h3>
-    <h3 class="block">Nos coloriages</h3>
-    </div>
-  </nav>
+  <header class="mb-7 flex bg-gray-100">
+    <nav class="flex">
+      <IconLogo class="mx-8 my-5 h-10 w-12" />
+
+      <div class="mx-6 mt-7 flex space-x-8 font-roboto">
+        <a href="AccueilView.vue"><h3 class="block">Acceuil</h3></a>
+        <a href="apropos"><h3 class="block">À propos</h3></a>
+        <a href="contact"><h3 class="block">Contact</h3></a>
+        <a href="nosartistes"><h3 class="block">Nos artistes</h3></a>
+        <a href="coloriages"><h3 class="block">Nos coloriages</h3></a>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
---
+++ src/views/ColoriageView.vue
@@ -0,0 +1,7 @@
+<template>
+  <img src="src/assets/images/hero_coloriage.jpg" alt="image hero" />
+  <p>Des feutres, des crayons de couleurs, et c’est parti ! Coloriez puis Téléchargez gratuitement des coloriages pour vos enfants !</p>
+  <div>
+    <img src="src/assets/images/" alt="" />
+  </div>
+</template>
