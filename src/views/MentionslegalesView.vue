9bc46557df7ab347d43e9e5e035823f7107adeed
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-23 21:23:06 +0200

    mentions légales marion

---
+++ src/views/MentionslegalesView.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <img src="../assets/images/test.jpg" alt="bandeau mentions légales" />
+
+    <h2 class="mx-24 mt-20 font-bold">DIRECTION DE LA PUBLICATION</h2>
+    <ul class="mx-24 mt-5">
+      <li>Kids'Art</li>
+      <li>Tél : 00 00 00 00 00</li>
+      <li>Mail : kidsart@gmail.com</li>
+    </ul>
+
+    <h2 class="mx-24 mt-20 font-bold">HÉBERGEMENT</h2>
+    <P class="mx-24 mt-5">Infomaniak : https://www.infomaniak.com/en?utm_term=60e4a39f57fef</P>
+
+    <h2 class="mx-24 mt-20 font-bold">DIRECTION DE LA PUBLICATION</h2>
+    <ul class="mx-24 mt-5">
+      <li>Pour le nom de domaine : morganeindustry.com</li>
+      <li>Pour le sous domaine : kidsart</li>
+    </ul>
+
+    <h2 class="mx-24 mt-20 font-bold">DROIT D'AUTEUR</h2>
+    <P class="mx-24 mt-5"
+      >Kids’Art propose ses servives à nos utilisateurs en toute légalité et respect des normes. Toutes correspondances avec notre site ou
+      exploitation commerciale n’est autorisé sans l’accord de Kids’Art. Tous les éléments présents dans le site Kids’Art sont strictement
+      sous la propriété de Kids’Art.
+    </P>
+    <li class="mx-32 mb-16">Droit d'auteur</li>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
