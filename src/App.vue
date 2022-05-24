3af32e611ad837023c6e0025bf141fecd0bae9da
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-05-24 17:19:49 +0200

    page coloriage

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
       <IconLogo class="mx-8 my-5 h-10 w-12" />
 
       <div class="mx-6 mt-7 flex space-x-8 font-roboto">
-        <a href="AccueilView.vue"><h3 class="block">Acceuil</h3></a>
+        <a href="accueil"><h3 class="block">Acceuil</h3></a>
         <a href="apropos"><h3 class="block">À propos</h3></a>
         <a href="contact"><h3 class="block">Contact</h3></a>
         <a href="nosartistes"><h3 class="block">Nos artistes</h3></a>
-        <a href="coloriages"><h3 class="block">Nos coloriages</h3></a>
+        <a href="coloriage"><h3 class="block">Nos coloriages</h3></a>
       </div>
     </nav>
   </header>
--- src/views/ColoriageView.vue
+++ src/views/ColoriageView.vue
@@ -1,7 +1,56 @@
 <template>
   <img src="src/assets/images/hero_coloriage.jpg" alt="image hero" />
   <p>Des feutres, des crayons de couleurs, et c’est parti ! Coloriez puis Téléchargez gratuitement des coloriages pour vos enfants !</p>
-  <div>
-    <img src="src/assets/images/" alt="" />
+  <div class="grid grid-cols-3">
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/images.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/images-2.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/images-3.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/images-4.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/images-5.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/image-6.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/image-7.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/image-8.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/images-9.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/image-10.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/image-11.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
+    <div class="w-40 bg-orange-fonce">
+      <img src="src/assets/images/image-13.jpg" alt="dessin 1" />
+      <p>colorier gratuitement</p>
+    </div>
   </div>
 </template>
+
+
