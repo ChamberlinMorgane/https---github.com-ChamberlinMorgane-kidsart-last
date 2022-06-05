a2ef8f946cc83057063e85deb06d2cebf1187a3c
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-06-05 18:07:42 +0200

    mise en place de card

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img class="rounded-sm" :src="image" alt="artiste" />
+    <div class="mt-3 mr-20 grid grid-cols-1 justify-items-center">
+      <p>{{ nom }}</p>
+      <p>{{ metier }}</p>
+      <button type="button" class="m-auto rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white focus:outline-none">
+        En savoir plus
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  /* Les "props" servent à définir les "attributs" qui seront passés a l'instance du composant pour le personnalisé.
+  Chaque "props" a un nom et un type */
+  props: {
+    nom: String,
+    image: String,
+    metier: String,
+  },
+  components: {},
+};
+</script>
---
+++ src/components/Card2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mt-10 mb-20 h-56 w-48 border-8 border-orange-fonce bg-orange-fonce">
+    <img class="rounded-sm" :src="image" alt="coloriage" />>
+    <div class="mt-2 flex h-5 w-5 flex-row gap-7">
+      <PencilAltIcon />
+      <p class="text-xs">colorier gratuitement</p>
+      <HeartIcone />
+    </div>
+    <a href="src/assets/images/images-2.pdf" download rel="noopener noreferrer" target="_blank"
+      ><img class="float-right mt-5 h-5 w-5" src="src/assets/icons/telecharger.png" alt="#"
+    /></a>
+  </div>
+</template>
+
+<script>
+import { PencilAltIcon, HeartIcon } from "@heroicons/vue/outline";
+export default {
+  /* Les "props" servent à définir les "attributs" qui seront passés a l'instance du composant pour le personnalisé.
+  Chaque "props" a un nom et un type */
+  props: {
+    nom: String,
+    image: String,
+    id: String,
+  },
+  components: {
+    PencilAltIcon,
+    HeartIcon,
+  },
+};
+</script>
