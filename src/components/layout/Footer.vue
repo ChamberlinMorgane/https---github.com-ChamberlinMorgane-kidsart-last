c5ff961709b7234b1b5c46e651e9c26010a56a90
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-06-05 18:08:25 +0200

    essaie footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="h-auto bg-black">
+    <nav class="grid grid-cols-5">
+      <ul class="mt-9">
+        <h5 class="mb-5">Kid’sArt</h5>
+        <li>Accueil</li>
+        <li>Contact</li>
+        <li>A propos</li>
+        <li>S'inscrire</li>
+        <li>Me connecter</li>
+        <li>Mon compte</li>
+      </ul>
+
+      <ul class="mt-9">
+        <h5 class="mb-5">A propos</h5>
+        <li>Qui sommes-nous ?</li>
+        <li>Pourquoi cette innovation</li>
+        <li>Pour nos utilisateurs</li>
+      </ul>
+
+      <ul class="mt-9">
+        <h5 class="mb-5">Contact</h5>
+        <li>Nous contacter</li>
+        <li>Nos artistes</li>
+        <li>Portection de la vie privée</li>
+      </ul>
+
+      <ul class="mt-9">
+        <h5 class="mb-5">Remboursement</h5>
+        <li>Achats en ligne</li>
+        <li>Remboursement</li>
+        <li>Mode de paiement</li>
+      </ul>
+    </nav>
+
+    <div class="float-right grid grid-cols-1 gap-6">
+      <div class="grid-flow-row2 grid">
+        <div class="mb-3 flex h-12 gap-8">
+          <img src="/img/logo-mastercard.jpg" />
+          <img src="/img/logo-paypal.jpg" />
+
+          <div>
+            <div>
+              <a href="https://www.facebook.com/profile.php?id=100009306961064" aria-label="Facebook">
+                <img src="/icon/facebook.png" alt="icon facebook"
+              /></a>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/in/marion-gillot-8133a7224/" aria-label="Instagram">
+                <img src="/icon/instagram.png" alt="icon instagram"
+              /></a>
+              <div>
+                <a href="https://twitter.com/1820Tc" aria-label="Twitter"> <img src="/icon/twitter.png" alt="icon twitter" /></a>
+              </div>
+              <div class="w-6">
+                <a href="https://www.linkedin.com/in/marion-gillot-8133a7224/" aria-label="Linlkedin">
+                  <img src="/icon/mmi.svg" alt="icon mmi"
+                /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-7 h-12 bg-black">
+      <nav class="flex">
+        <div class="font-regular font-regular mx-6 flex space-x-8 p-3">
+          <h3 class="block"><a href="mentionslegales">Mentions légales</a></h3>
+          <h3 class="block">Cookies</h3>
+          <h3 class="block">Politique de protection des données personnelles</h3>
+          <h3 class="block">Plan du site</h3>
+          <h3 class="block">© Kid’sArt FRANCE 2022 Crée par Marion Gillot et Morgane Chamberlin</h3>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
