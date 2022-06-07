98a159afc4873f7e6fdff95140bf73fa5641cd41
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-06-07 11:44:13 +0200

    responsive page a propos

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -1,35 +1,40 @@
 <template>
-  <img src="src/assets/images/hero_apropos.jpg" alt="hero a propos" />
+  <img class="mt-48 w-full" src="src/assets/images/hero_apropos.jpg" alt="hero a propos" />
   <h4 class="mt-12 mb-12 flex justify-center text-2xl">Qui sommes nous ?</h4>
-  <img class="ml-72 mb-5 h-2" src="src/assets/images/apropos-bas.jpg" alt="petite bare texte" />
-  <div class="flex w-64 flex-nowrap justify-self-center">
-    <img src="src/assets/images/image-avatar-morgane.jpg" alt="image bonhomme morgane" />
-    <img src="src/assets/images/image-avatar-marion.jpg" alt="image bonhomme marion" />
-  </div>
-  <div class="flex flex-nowrap justify-center gap-28">
-    <p class="">Morgane CHAMBERLIN</p>
-    <p>Marion GILLOT</p>
+  <img class="m-auto mb-4 flex h-2 justify-center" src="src/assets/images/apropos-bas.jpg" alt="petite bare texte" />
+  <div class="grid grid-cols-1 justify-items-center lg:grid-cols-2">
+    <div>
+      <img src="src/assets/images/image-avatar-morgane.jpg" alt="image bonhomme morgane" />
+      <p class="m-auto flex justify-center lg:text-xl">Morgane CHAMBERLIN</p>
+    </div>
+    <div>
+      <img src="src/assets/images/image-avatar-marion.jpg" alt="image bonhomme marion" />
+      <p class="m-auto flex justify-center lg:text-xl">Marion GILLOT</p>
+    </div>
   </div>
 
-  <p class="mr-20 ml-20 mt-10">
+  <p class="m-auto mr-20 mt-10 flex justify-center text-center lg:text-xl">
     Nous sommes Marion GILLOT et Morgane CHAMBERLIN, toutes deux étudiantes en première année MMi, Métier du Multimédia et de l'Internet à
     l'IUT de Montbeliard(25)
   </p>
   <h4 class="mb-5 mt-8 flex justify-center text-2xl">Pourquoi cette innovation</h4>
-  <p class="mr-20 ml-20 mt-10">
+  <img class="m-auto mb-4 flex h-2 w-64 justify-center" src="src/assets/images/apropos-bas.jpg" alt="petite bare texte" />
+
+  <p class="m-auto mt-10 flex justify-center text-center lg:text-xl">
     Fait dans le cadre d'un exercice pédagogique, dont le sujet est de créer une application innovante. C'est pourquoi nous avons penser
     lier familles et artistes afin d'apporter au dessin de vos enfants, la créativité des artistes.
   </p>
   <h4 class="mb-3 mt-8 flex justify-center text-2xl">Comment assurons-nous que ce site reste sur pour nos utilisateurs</h4>
-  <p class="ml-14 mr-14 mt-10">
+  <img class="m-auto mb-4 flex h-2 w-2/6 justify-center" src="src/assets/images/apropos-bas.jpg" alt="petite bare texte" />
+  <p class="m-auto mt-10 flex justify-center text-center lg:text-xl">
     Nous surveillons les activités sur le site pour la sécurité de chacun. Malheureusement, comme nous ne pouvons pas être partout en même
     temps nous avons d'autres mesures:
   </p>
-  <p>
+  <p class="m-auto mt-10 flex justify-center text-center lg:text-xl">
     Nous demandons aux utilisateurs de signaler toute personne qui utilise un language ou des images blessantes ou inapropriés (commentaire,
     message, noms d'utilisateurs, avatars, créations.)
   </p>
-  <p>
+  <p class="m-auto mt-10 mb-11 flex justify-center text-center lg:text-xl">
     Votre enfant devrait égaleemnt utiliser des filtres de language pour l'empêcher d'afficher tout contenu qui pourrait venir d'autres
     utilisateurs
   </p>
