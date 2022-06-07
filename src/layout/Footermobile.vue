80afc317d52946583d4bc6c7e0067070476fb556
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-06-07 18:20:37 +0200

    deplacement image

--- src/layout/Footermobile.vue
+++ src/layout/Footermobile.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-fond1footer h-96 p-3">
+  <div class="h-96 bg-fond1footer p-3">
     <div>
-        <RouterLink to="/"><Logo class="w-12 h-11 mx-7 my-5" /></RouterLink>
+      <RouterLink to="/"><Logo class="mx-7 my-5 h-11 w-12" /></RouterLink>
     </div>
 
     <div class="mx-7 mb-6">
@@ -13,16 +13,16 @@
     <div class="mx-7 flex gap-6">
       <div>
         <a href="https://www.facebook.com/profile.php?id=100009306961064" aria-label="Facebook">
-          <img src="/icons/facebook.png" alt="icons facebook"
+          <img src="/icons/facebook.svg" alt="icons facebook"
         /></a>
       </div>
       <div>
         <a href="https://www.linkedin.com/in/marion-gillot-8133a7224/" aria-label="Instagram">
-          <img src="/icons/instagram.png" alt="icons instagram"
+          <img src="/icons/instagram.svg" alt="icons instagram"
         /></a>
       </div>
       <div>
-        <a href="https://twitter.com/1820Tc" aria-label="Twitter"> <img src="/icons/twitter.png" alt="icons twitter" /></a>
+        <a href="https://twitter.com/1820Tc" aria-label="Twitter"> <img src="/icons/twitter.svg" alt="icons twitter" /></a>
       </div>
       <div class="w-6">
         <a href="https://www.linkedin.com/in/marion-gillot-8133a7224/" aria-label="Linlkedin">
