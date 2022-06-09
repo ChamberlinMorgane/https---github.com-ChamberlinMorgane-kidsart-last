423f313bc83c8c367a9c63917eab21c250a7a7fe
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2022-06-09 10:30:39 +0200

    lien guide de style

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -70,12 +70,13 @@
 
     <div class="mt-7 h-12 bg-fond2footer">
       <nav class="flex">
-        <div class="font-regular font-regular mx-6 flex space-x-8 p-3">
+        <div class="font-regular mx-6 flex space-x-8 p-3 text-sm">
           <h3 class="block"><a href="mentionslegales">Mentions légales</a></h3>
           <h3 class="block">Cookies</h3>
           <h3 class="block">Politique de protection des données personnelles</h3>
           <h3 class="block">Plan du site</h3>
           <h3 class="block">© Kid’sArt FRANCE 2022 Crée par Marion Gillot et Morgane Chamberlin</h3>
+          <h3 class="block"><a href="guidestyle">Guide de style </a></h3>
         </div>
       </nav>
     </div>
